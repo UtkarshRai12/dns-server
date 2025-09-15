566700e055801793dbe91f0bb852eb8810856278
utkarsh rai <utkarshrai5678@gmail.com>
2025-09-15 17:38:39 +0530

    added sample header

--- app/main.js
+++ app/main.js
@@ -39,11 +39,7 @@ udpSocket.on("message", (buf, rinfo) => {
       "responseBuffer",
       Buffer.concat([responseHeader, questionBuffer, answerBuffer])
     );
-    udpSocket.send(
-      Buffer.concat([responseHeader, questionBuffer]),
-      rinfo.port,
-      rinfo.address
-    );
+    udpSocket.send(Buffer.concat([responseHeader]), rinfo.port, rinfo.address);
   } catch (e) {
     console.log(`Error receiving data: ${e}`);
   }
