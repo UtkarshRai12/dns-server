c04e7b98726e80f4ada5697e8faf4cf59a3ab87c
utkarsh rai <utkarshrai5678@gmail.com>
2025-09-15 17:18:36 +0530

    added sample header

--- app/main.js
+++ app/main.js
@@ -13,7 +13,7 @@ udpSocket.on("message", (buf, rinfo) => {
     responseHeader[2] = responseHeader[2] & 0xff;
     responseHeader[3] = responseHeader[3] | 0x04;
     responseHeader[5] = responseHeader[5] | 0x01;
-    // responseHeader[7] = responseHeader[7] | 0x01;
+    responseHeader[7] = responseHeader[7] | 0x01;
     let questionBuffer = Buffer.from([]);
     console.log("header:", responseHeader);
     let curr = 12;
